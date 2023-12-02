fd83301f88847f10674a22727a66c535c3cbf990
spenceryang <yangzijian@gmail.com>
2023-12-02 18:28:14 +0000

    added wbtc features

--- src/pages/earnPage.js
+++ src/pages/earnPage.js
@@ -18,7 +18,8 @@ let web3;
 export default function EarnPage() {
   const infoToPass = useContext(GlobalContext);
   const [account, setAccount] = useState(null);
-  const [amount, setAmount] = useState("");
+  const [wethAmount, setWethAmount] = useState("");
+  const [wbtcAmount, setWbtcAmount] = useState("");
   const [balance, setBalance] = useState("0");
   const [transactions, setTransactions] = useState([]);
 
@@ -50,12 +51,16 @@ export default function EarnPage() {
     setTransactions([]);
   };
 
-  const handleAmountChange = (e) => {
-    setAmount(e.target.value);
+  const handleWethAmountChange = (e) => {
+    setWethAmount(e.target.value);
+  };
+
+  const handleWbtcAmountChange = (e) => {
+    setWbtcAmount(e.target.value);
   };
 
   const supplyWETH = async () => {
-    if (!account || !amount) return;
+    if (!account || !wethAmount) return;
 
     try {
       const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
@@ -68,7 +73,7 @@ export default function EarnPage() {
       );
 
       // Approve the LendingPool contract to spend your WETH
-      const amountInWei = web3.utils.toWei(amount, "ether");
+      const amountInWei = web3.utils.toWei(wethAmount, "ether");
       await wethContract.methods
         .approve(lendingPoolAddress, amountInWei)
         .send({ from: account });
@@ -78,14 +83,14 @@ export default function EarnPage() {
         .deposit(wethTokenAddress, amountInWei, account, 0)
         .send({ from: account });
 
-      console.log("Deposited", amount, "WETH to Aave v3");
+      console.log("Deposited", wethAmount, "WETH to Aave v3");
     } catch (error) {
       console.error("Error in supplying WETH:", error);
     }
   };
 
   const withdrawWETH = async () => {
-    if (!account || !amount) return;
+    if (!account || !wethAmount) return;
 
     try {
       const wethTokenAddress = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
@@ -97,16 +102,74 @@ export default function EarnPage() {
       );
 
       // Convert the amount to Wei
-      const amountInWei = web3.utils.toWei(amount, "ether");
+      const amountInWei = web3.utils.toWei(wethAmount, "ether");
 
       // Withdraw WETH from the LendingPool
       await lendingPoolContract.methods
         .withdraw(wethTokenAddress, amountInWei, account)
         .send({ from: account });
 
-      console.log("Withdrew", amount, "WETH from Aave v3");
+      console.log("Withdrew", wethAmount, "WETH from Aave v3");
     } catch (error) {
       console.error("Error in withdrawing WETH:", error);
+    }
+  };
+
+  const toSatoshi = (amount) => {
+    // WBTC has 8 decimal places
+    // Multiply the amount by 10^8 to convert to satoshi
+    return (parseFloat(amount) * Math.pow(10, 8)).toString();
+  };
+
+  const supplyWBTC = async () => {
+    if (!account || !wbtcAmount) return;
+
+    try {
+      const wbtcTokenAddress = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+      const lendingPoolAddress = "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2";
+
+      const wbtcContract = new web3.eth.Contract(erc20ABI, wbtcTokenAddress);
+      const lendingPoolContract = new web3.eth.Contract(
+        aaveLendingPoolABI,
+        lendingPoolAddress,
+      );
+
+      // Convert the amount to satoshi for WBTC
+      const amountInSatoshi = toSatoshi(wbtcAmount);
+      await wbtcContract.methods
+        .approve(lendingPoolAddress, amountInSatoshi)
+        .send({ from: account });
+
+      await lendingPoolContract.methods
+        .deposit(wbtcTokenAddress, amountInSatoshi, account, 0)
+        .send({ from: account });
+
+      console.log("Deposited", wbtcAmount, "WBTC to Aave v3");
+    } catch (error) {
+      console.error("Error in supplying WBTC:", error);
+    }
+  };
+
+  const withdrawWBTC = async () => {
+    if (!account || !wbtcAmount) return;
+
+    try {
+      const wbtcTokenAddress = "0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599";
+      const lendingPoolAddress = "0x87870Bca3F3fD6335C3F4ce8392D69350B4fA4E2";
+
+      const lendingPoolContract = new web3.eth.Contract(
+        aaveLendingPoolABI,
+        lendingPoolAddress,
+      );
+
+      const amountInSatoshi = toSatoshi(wbtcAmount);
+      await lendingPoolContract.methods
+        .withdraw(wbtcTokenAddress, amountInSatoshi, account)
+        .send({ from: account });
+
+      console.log("Withdrew", wbtcAmount, "WBTC from Aave v3");
+    } catch (error) {
+      console.error("Error in withdrawing WBTC:", error);
     }
   };
 
@@ -134,6 +197,37 @@ export default function EarnPage() {
     }
   };
 
+  const ProductCard = ({ title, onDeposit, onWithdraw, onChange, amount }) => {
+    return (
+      <div className="rounded-lg bg-white px-4 py-5 shadow sm:p-6">
+        <h3 className="text-lg font-semibold text-gray-900">{title}</h3>
+        <div className="mt-4">
+          <input
+            type="text"
+            value={amount}
+            onChange={onChange}
+            className="w-full rounded-md border border-gray-300 px-3 py-2"
+            placeholder="Amount"
+          />
+          <div className="mt-4 flex justify-between">
+            <button
+              onClick={onDeposit}
+              className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+            >
+              Deposit
+            </button>
+            <button
+              onClick={onWithdraw}
+              className="rounded-md bg-red-600 px-4 py-2 text-white hover:bg-red-700"
+            >
+              Withdraw
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="flex flex-col">
       <h1 className="p-0 text-3xl font-bold text-black">EarnPage</h1>
@@ -145,7 +239,7 @@ export default function EarnPage() {
       {account && (
         <div>
           <h2>Wallet Address: {account}</h2>
-          <h2>Wallet Balance: {balance} ETH</h2>
+          <h2>Wallet ETH Balance: {balance} ETH</h2>
           <h3>Last 2 Transactions:</h3>
           <ul>
             {transactions.map((tx, index) => (
@@ -154,17 +248,24 @@ export default function EarnPage() {
               </li>
             ))}
           </ul>
-          <input
-            type="text"
-            value={amount}
-            onChange={handleAmountChange}
-            placeholder="Amount of WETH"
-          />
-          <button onClick={supplyWETH}>Supply WETH to Aave v3</button>
-          <button onClick={withdrawWETH}>Withdraw WETH from Aave v3</button>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-2">
+            <ProductCard
+              title="WETH"
+              amount={wethAmount}
+              onChange={handleWethAmountChange}
+              onDeposit={supplyWETH}
+              onWithdraw={withdrawWETH}
+            />
+            <ProductCard
+              title="WBTC"
+              amount={wbtcAmount}
+              onChange={handleWbtcAmountChange}
+              onDeposit={supplyWBTC}
+              onWithdraw={withdrawWBTC}
+            />
+          </div>
         </div>
       )}
-      {/* ... Other components ... */}
     </div>
   );
 }
