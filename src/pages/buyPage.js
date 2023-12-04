2634a5084fd250f87d480d38605e0340d3c861cc
spenceryang <yangzijian@gmail.com>
2023-12-04 09:52:57 +0000

    added moonpay buy button

--- src/pages/buyPage.js
+++ src/pages/buyPage.js
@@ -2,14 +2,31 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../providers/globalProvider.js";
+import { MoonPayBuyWidget } from "@moonpay/moonpay-react";
 
 export default function BuyPage() {
   const infoToPass = useContext(GlobalContext);
   const navigate = useNavigate();
+  const [visible, setVisible] = useState(false);
 
   return (
     <div className="flex flex-row">
       <h1 className="p-0 text-3xl font-bold text-black">BuyPage</h1>
+      <div>
+        <MoonPayBuyWidget
+          variant="embedded"
+          baseCurrencyCode="usd"
+          baseCurrencyAmount="100"
+          defaultCurrencyCode="eth"
+          visible={visible}
+        />
+        <button
+          className="rounded-md bg-blue-600 px-4 py-2 text-white hover:bg-blue-700"
+          onClick={() => setVisible(!visible)}
+        >
+          Buy Crypto with MoonPay
+        </button>
+      </div>
     </div>
   );
 }
