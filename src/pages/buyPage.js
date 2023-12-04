77144f18216bdc0010d5b5d9e5563c637ea383e5
KH-Pua <keehui.pua@gmail.com>
2023-12-04 15:16:24 +0800

    build: buyPage initial setup

--- src/pages/buyPage.js
+++ src/pages/buyPage.js
@@ -1,11 +1,24 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../providers/globalProvider.js";
-import NavBar from "../components/details/NavBar.js";
+import { loadMoonPay } from "@moonpay/moonpay-js";
 
 export default function BuyPage() {
   const infoToPass = useContext(GlobalContext);
   const navigate = useNavigate();
+  // const moonPay = await loadMoonPay();
+
+  // const moonPaySdk = moonPay({
+  //   flow: 'buy', // possible values for flow: buy, sell, swapsCustomerSetup
+  //   environment: 'sandbox', // possible values for environment: sandbox, production
+  //   varient: 'overlay', // possible values for varient: embedded, overlay, newTab, newWindow
+  //   params: {
+  //     apiKey: 'pk_test_key'
+  //   },
+  //   debug: true
+  // })
+
+
 
   return (
     <div className="flex flex-row">
