2feaf29489d2f6acbf865a07d0549d954e6336eb
shanquanSQ <thiashanquan93@gmail.com>
2023-12-06 12:11:58 +0800

    polish dashboard pt1

--- src/pages/dashboardPage.js
+++ src/pages/dashboardPage.js
@@ -2,6 +2,7 @@
 import { useState, useEffect, useContext } from "react";
 import { useNavigate } from "react-router-dom";
 import { GlobalContext } from "../providers/globalProvider.js";
+import axios from "axios";
 
 // Web3 Imports - Can be refactored in the future after we got all the methods out
 import Web3 from "web3";
@@ -22,6 +23,8 @@ const settings = {
 const alchemy = new Alchemy(settings);
 let web3;
 
+const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
+
 export default function DashboardPage() {
   const infoToPass = useContext(GlobalContext);
   const navigate = useNavigate();
@@ -35,6 +38,10 @@ export default function DashboardPage() {
   const [tokenBalance, setTokenBalance] = useState(null);
   const [coinImage, setCoinImage] = useState({});
   const [imagesFlag, setImagesFlag] = useState(false);
+
+  // Total Holdings and Total Earnings
+  const [totalHoldings, setTotalHoldings] = useState(null);
+  const [totalEarnings, setTotalEarnings] = useState(null);
 
   useEffect(() => {
     if (window.ethereum) {
@@ -53,21 +60,96 @@ export default function DashboardPage() {
   useEffect(() => {
     if (account) {
       fetchBalance(account);
+      getUserTotalHoldings();
     }
 
-    // Prevent duplicate calls.
     if (account && tokenBalance == null) {
       getWalletAaveSupportedCoins(account);
     }
   }, [account]);
 
+  const getUserTotalHoldings = async () => {
+    console.log(`enter getUserTotalHoldings`);
+    let userId = 1; // Need to get from base template
+
+    // Get User's past transactions on bitjar
+    const pastBitjarTransactions = await axios.post(
+      `${BACKEND_URL}/users/getUserPastTransactions`,
+      { userId: userId },
+    );
+
+    const userPastBitjarTransactions = pastBitjarTransactions.data.data;
+
+    let coinSymbolsList = [];
+    let walletCoinAmount = {};
+
+    // Get Transacted Coin Symbol and Coin Amounts
+    for (const coin in userPastBitjarTransactions) {
+      let coinSymbol =
+        userPastBitjarTransactions[coin].product.productName.split(" ")[0];
+      coinSymbolsList.push(coinSymbol);
+      walletCoinAmount[coinSymbol] = userPastBitjarTransactions[coin].amount;
+    }
+
+    console.log(`walletCoinAmount is ${JSON.stringify(walletCoinAmount)}`);
+    console.log(`coinSymbolsList is ${coinSymbolsList}`);
+
+    let coinData = {};
+
+    // Get Latest Coin Information from CMC
+    console.log("Running CMC API Calls?");
+    await Promise.all(
+      Object.keys(coinSymbolsList).map(async (symbol) => {
+        // console.log(`symbol is ${symbol}`);
+        // console.log(`the symbol is: "${coinSymbolsList[symbol]}"`);
+        let information = await axios.post(
+          `${BACKEND_URL}/users/getCoinLatestInfo`,
+          {
+            coinSYM: coinSymbolsList[symbol],
+          },
+        );
+        // console.log(`information is: ${JSON.stringify(information.data.data)}`);
+        coinData[coinSymbolsList[symbol]] = information.data.data;
+      }),
+    );
+
+    let latestCoinPrices = {};
+    // Filter latest price from the above set state.
+    // for (const key in coinLatestData) {
+    for (const key in coinData) {
+      // console.log(`${key}`);
+      // console.log(
+      //   coinData[`${key}`].data[`${key}`.toUpperCase()].quote.USD.price,
+      // );
+
+      let price =
+        coinData[`${key}`].data[`${key}`.toUpperCase()].quote.USD.price;
+
+      latestCoinPrices[`${key}`] = price;
+    }
+
+    // Calculate Total Holdings
+    let totalHoldings = 0.0;
+    // console.log(
+    //   `initialise calculateTotalHoldings for: ${JSON.stringify(
+    //     walletCoinAmount,
+    //   )} and ${JSON.stringify(latestCoinPrices)}`,
+    // );
+    for (const key in walletCoinAmount) {
+      // console.log(`The value of ${key} held is: `, coinAmount[`${key}`]);
+      let sum = walletCoinAmount[`${key}`] * latestCoinPrices[`${key}`];
+      // console.log(sum);
+      totalHoldings += sum;
+    }
+    // console.log("total holdings is:", parseFloat(totalHoldings.toFixed(2)));
+    let holdings = parseFloat(totalHoldings.toFixed(2));
+    setTotalHoldings(holdings);
+  };
+
   const fetchBalance = async (address) => {
     try {
-      console.log("Fetching Balance");
       const balanceWei = await web3.eth.getBalance(address);
-      console.log(`balance in wei: ${balanceWei}`);
       const balanceEth = web3.utils.fromWei(balanceWei, "ether");
-      console.log(`balance in Eth: ${balanceWei}`);
       setBalance(balanceEth);
     } catch (error) {
       console.error("Error fetching balance:", error);
@@ -160,8 +242,10 @@ export default function DashboardPage() {
 
   return (
     <div className="flex w-full flex-row ">
-      <div className="flex w-full flex-col justify-center gap-[.5em]">
-        <h1 className="p-0 text-3xl font-bold text-black">Dashboard</h1>
+      <div className="flex w-full flex-col justify-center gap-[.5em] px-3">
+        <h1 className="p-0 pb-[1em] text-3xl font-bold text-black">
+          Dashboard
+        </h1>
         <div>
           {!account ? (
             <button onClick={connectWallet} className="connectWalletBtn">
@@ -173,7 +257,6 @@ export default function DashboardPage() {
             </button>
           )}
         </div>
-
         {/* User Primary Information */}
         {!account ? null : (
           <div className="flex w-full flex-row justify-start gap-[3em] pb-[2em]">
@@ -182,7 +265,9 @@ export default function DashboardPage() {
               <p className="text-[.7rem] font-semibold text-slate-400">
                 with BitJar
               </p>
-              <p className="text-[2rem] font-semibold"> xxx USD</p>
+              <p className="text-[2rem] font-semibold">
+                {totalHoldings != null && totalHoldings} USD
+              </p>
             </div>
             <div>
               <h2 className="font-semibold text-slate-700">Total Earnings:</h2>
@@ -193,12 +278,11 @@ export default function DashboardPage() {
             </div>
           </div>
         )}
-
         {/* User's Assets */}
         {!account ? null : (
           <>
             <div className=" pb-[2em]">
-              <h1 className="text-xl font-bold">Assets</h1>
+              <h1 className="pb-[1em] text-xl font-bold">Assets</h1>
               <div>
                 <h2 className="font-semibold text-slate-700">
                   Wallet Balance:
@@ -214,34 +298,38 @@ export default function DashboardPage() {
             </div>
           </>
         )}
-
         {!account ? null : (
           <div className="h-full w-full pb-[1em]">
             <h2 className="pb-[1em] font-semibold text-slate-700">
               Supported Coin Balances:
             </h2>
-            <div className="flex h-[12em] w-full flex-col flex-wrap justify-start gap-x-[.5em] gap-y-[.5em] overflow-x-scroll ">
+            {/* Not sure how to express the Height as a proportion, % didnt work.. Hardcoded it for now */}
+            <div className="flex h-[13em] w-full flex-col flex-wrap justify-start gap-x-[.5em] gap-y-[.5em] overflow-x-scroll ">
               {imagesFlag && coinImage
-                ? // && account
-                  walletTokens.map((element, index) => {
+                ? walletTokens.map((element, index) => {
                     console.log(`element is ${element}`);
                     console.log(coinImage[element]);
                     return (
                       <div
                         key={element}
-                        className="flex w-[10%] flex-col items-center rounded-md border bg-slate-100 py-[1em]"
+                        className="flex min-w-[10em] flex-col items-center rounded-md border bg-slate-100 py-[1em] lg:w-[8em]"
                       >
-                        <div className="font-semibold">{element}</div>
+                        <div className="pb-[1em] font-bold">{element}</div>
                         {coinImage[element] ? (
                           <TokenCard imagesrc={coinImage[element]} />
                         ) : (
                           <TokenCard imagesrc="https://icon-library.com/images/cancel-icon-transparent/cancel-icon-transparent-5.jpg" />
                         )}
-                        <p className="text-[.7rem] font-semibold text-slate-400">
+                        <p className="pt-[1em] text-[.7rem] font-semibold text-slate-500">
                           Balance:
                         </p>
                         <p>
-                          {tokenBalance[element]} {element}
+                          <span className="inline text-[1rem] font-semibold">
+                            {tokenBalance[element]}{" "}
+                          </span>
+                          <span className="inline text-[.8rem] font-bold">
+                            {element}
+                          </span>
                         </p>
                       </div>
                     );
@@ -250,11 +338,10 @@ export default function DashboardPage() {
             </div>
           </div>
         )}
-
         {/* User's Transactions on BitJar */}
         {!account ? null : (
           <div className="pb-[2em]">
-            <h1 className="text-xl font-bold">Transactions</h1>
+            <h1 className="pb-[1em] text-xl font-bold">Transactions</h1>
             <figure>
               <TransactionHistoryTable userId="1" />
             </figure>
