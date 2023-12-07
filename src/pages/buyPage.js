0d4390b04c478407320eca78eb9f28d32cafd91c
shanquanSQ <thiashanquan93@gmail.com>
2023-12-07 11:54:32 +0800

    stuff

--- src/pages/buyPage.js
+++ src/pages/buyPage.js
@@ -8,16 +8,18 @@ export default function BuyPage() {
   return (
     <div className="flex flex-row">
       <h1 className="p-0 text-3xl font-bold text-black">Buy</h1>
-      <div>
-        <MoonPayBuyWidget
-          variant="embedded"
-          baseCurrencyCode="usd"
-          baseCurrencyAmount="100"
-          defaultCurrencyCode="eth"
-          walletAddress={address}
-          colorCode="#face5e"
-        />
-      </div>
+      {address ? (
+        <div>
+          <MoonPayBuyWidget
+            variant="embedded"
+            baseCurrencyCode="usd"
+            baseCurrencyAmount="100"
+            defaultCurrencyCode="eth"
+            walletAddress={address}
+            colorCode="#face5e"
+          />
+        </div>
+      ) : null}
     </div>
   );
 }
